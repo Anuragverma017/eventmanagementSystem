e330b9cd7f2d481a1b42111a267225594c0c1e35
Anuragverma017 <anuragverma1632004@gmail.com>
2024-04-04 16:12:02 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,166 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Bootstrap Web Design</title>
+        <?php require 'utils/styles.php'; ?><!--css links. file found in utils folder-->
+        <?php require 'utils/scripts.php'; ?><!--js links. file found in utils folder-->
+    </head>
+    <body>
+        <?php require 'utils/header.php'; ?><!--header content. file found in utils folder-->
+        <div class = "content"><!--body content holder-->
+            <div class = "container">
+                <div class = "col-md-12"><!--body content title holder with 12 grid columns-->
+                    <h1>What we organize</h1><!--body content title-->
+                </div>
+            </div>
+			
+            <div class="container">
+            <div class="col-md-12">
+            <hr>
+            </div>
+            </div>
+			
+            <div class="row"><!--wedding content-->
+                <section>
+                    <div class="container">
+                        <div class="col-md-6"><!--image holder with 6 grid columns-->
+                            <img src="images/techimage.jpg" class="img-responsive" style="height:325px" width="488">
+                        </div>
+                        <div class="subcontent col-md-6"><!--Text holder with 6 column grid-->
+                            <h1>Technical Events</h1><!--title-->
+                            <p><!--content-->
+                            "Join us for an exhilarating tech extravaganza! Explore cutting-edge innovations, engage in interactive workshops, and network with industry leaders. Unleash your creativity and passion for technology at our dynamic college tech event. Don't miss out!".
+                            </p>
+                            <!-- <hr class="customline">css modified horizontal line -->
+                           
+                        </div><!--subcontent div-->
+                    </div><!--container div-->
+                </section>
+            </div><!--row div-->
+			
+            <div class="container">
+            <div class="col-md-12">
+            <hr>
+            </div>
+            </div>
+
+            <div class="row">
+                <section>
+                    <div class="container">
+                        <div class="col-md-6"><!--image holder with 6 grid columns-->
+                            <img src="images/outdoorimage.jpg" class="img-responsive" style="height:325px" width="488">
+                        </div>
+                        <div class="subcontent col-md-6"><!--Text holder with 6 column grid-->
+                            <h1>Out Door Games</h1><!--title-->
+                            <p><!--content-->"Embark on an adventure with our outdoor extravaganza! Join fellow students for a day of excitement, team challenges, and nature exploration. From thrilling games to serene hikes, experience the ultimate outdoor escape at our college event!"
+                            </p>
+                            <!-- <hr class="customline">css modified horizontal line -->
+
+                        </div><!--subcontent div-->
+                    </div><!--container div-->
+                </section>
+            </div><!--row div-->
+			
+            <div class="container">
+            <div class="col-md-12">
+            <hr>
+            </div>
+            </div>
+
+            <div class="row">
+                <section>
+                    <div class="container">
+                        <div class="col-md-6"><!--image holder with 6 grid columns-->
+                            <img src="images/indooriamge.webp" class="img-responsive" style="height:325px" width="488">
+                        </div>
+                        <div class="subcontent col-md-6"><!--Text holder with 6 column grid-->
+                            <h1>Indoor  Games</h1><!--title-->
+                            <p><!--content-->
+                            "Dive into the excitement of our indoor games extravaganza! Join us for a day packed with friendly competition, strategic challenges, and non-stop fun. From classic board games to virtual showdowns, there's something for everyone at our college event!"
+                            </p>
+                            <!-- <hr class="customline">css modified horizontal line -->
+                            
+                        </div><!--subcontent div-->
+                    </div><!--container div-->
+                </section>
+            </div><!--row div-->
+			
+            <div class="container">
+            <div class="col-md-12">
+                <hr>
+            </div>
+            </div>
+            
+            
+            <div class="row">
+                <section>
+                    <div class="container">
+                        <div class="col-md-6"><!--image holder with 6 grid columns-->
+                            <img src="images/wallpainting.jpeg" class="img-responsive" style="height:325px" width="488">
+                        </div>
+                        <div class="subcontent col-md-6"><!--Text holder with 6 column grid-->
+                            <h1>Wallpainting</h1><!--title-->
+                            <p><!--content-->
+                            "Express your creativity on campus walls! Join us for a colorful day of artistic exploration and community bonding. Unleash your imagination, grab a brush, and leave your mark on our college walls during this vibrant painting event."</p>
+                            <!-- <hr class="customline">css modified horizontal line -->
+                            
+                        </div><!--subcontent div-->
+                    </div><!--container div-->
+                </section>
+            </div><!--row div-->
+			
+            <div class="container">
+            <div class="col-md-12">
+                <hr>
+            </div>
+            </div>
+            
+            
+            <div class="row">
+                <section>
+                    <div class="container">
+                        <div class="col-md-6"><!--image holder with 6 grid columns-->
+                            <img src="images/singingimage.jpg" class="img-responsive" style="height:325px" width="488">
+                        </div>
+                        <div class="subcontent col-md-6"><!--Text holder with 6 column grid-->
+                            <h1>Singing</h1><!--title-->
+                            <p><!--content-->
+                           
+"Get ready to hit the high notes at our melodious singing event! Join fellow students for a soulful evening filled with captivating performances, harmonious tunes, and heartfelt lyrics. Let your voice shine on stage!"</p>
+                            <!-- <hr class="customline">css modified horizontal line -->
+                           
+                        </div><!--subcontent div-->
+                    </div><!--container div-->
+                </section>
+            </div><!--row div-->
+			
+            <div class="container">
+            <div class="col-md-12">
+                <hr>
+            </div>
+            </div>
+            
+
+            <div class="row">
+                <section>
+                    <div class="container">
+                        <div class="col-md-6"><!--image holder with 6 grid columns-->
+                            <img src="images/danceimage.jpg" class="img-responsive" style="height:325px" width="488">
+                        </div>
+                        <div class="subcontent col-md-6"><!--Text holder with 6 column grid-->
+                            <h1>Dance</h1><!--title-->
+                            <p><!--content-->"Feel the rhythm and ignite your spirit at our electrifying dance event! Join us for a night of pulsating beats, dazzling performances, and unforgettable moves. Let loose, showcase your talent, and groove with us on the dance floor!"
+                            </p>
+                            <!-- <hr class="customline">css modified horizontal line -->
+                            
+                        </div><!--subcontent div-->
+                    </div><!--container div-->
+                </section>
+            </div><!--row div-->
+
+            
+        </div><!--body content div-->
+        <?php require 'utils/footer.php'; ?><!--footer content. file found in utils folder-->
+    </body>
+</html>
---
+++ utils/footer.php
@@ -0,0 +1,38 @@
+<hr class = "footerline"><!--css modified horizontal line-->
+<footer>
+    <div class = "container">
+        <div class = "row">
+            <section>
+                <div class = "footerContent col-md-4"><!--left content-->
+                    <p class = "footerContent1">
+                        <strong>B</strong><span class = "small footerSubtext">ansal </span>
+                        <strong>C</strong><span class = "small footerSubtext">ollege of</span>
+                        <strong>E</strong><span class = "small footerSubtext">ngineering </span>
+                        <strong>M</strong><span class = "small footerSubtext">andideep</span>
+                    </p>
+
+                    <p class = "footerSubtext2">
+                        Hill view city,Old cannel, Itarsi, 
+                        &copy; Evecon Team, 2024.
+                    </p>
+                </div>
+            </section>
+            <section>
+                <div class = "footcontent col-md-4"><!--middle content-->
+                     Multimedia Design/Web Engineering<br>
+                    Creative Computing 2023-2024.<br>
+                    Anurag verma, No-01456470
+                </div>
+            </section>
+            <section>
+                <div class = "footcontent col-md-4"><!--right content-->
+                    Follow Us:<br>
+                        <img src = "images/facebook.png">
+                        <img src = "images/twitter.png">
+                        <img src = "images/googleplus.png">
+                        <img src = "images/youtube.png">
+                </div>
+            </section>
+        </div>
+    </div>
+</footer>
